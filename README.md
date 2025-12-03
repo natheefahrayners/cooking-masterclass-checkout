cf496d4ed4b51fe47a026b14885f12bcba21dc24
Natheefah Rayners <natheefahrayners4@gmail.com>
2025-12-03 15:40:55 +0200

    cooking-masterclass-checkout

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # cooking-masterclass-checkout
+
+## Features
+- Dynamic course catalogue displayed in card components.
+- Images, titles, instructors and descriptions included.
+- Clean, modern layout.
+- Fully responsive design.
+- Built with Vue 3 + Vite.
+
+## Project Setup
+1. Install Dependencies
+    npm install
+2. Start the development server
+    npm run dev
+    http://localhost:5173
+3. Build for production
+    npm run build
+
+## Project Structure
+src/
+|--components/
+|       |-- CourseCard.vue
+|       |-- HeaderBar.vue
+|
+|-- assets/
+|       |-- course-images/
+|
+|-- App.vue
+|-- main.js
+|-- data.js
+
+## Screenshot of Interface
+https://i.postimg.cc/sgKvktgq/screenshot-interface.png
---
+++ src/data/courses.js
@@ -0,0 +1,56 @@
+export default [
+{
+    id: 1,
+    title: "Classic Sushi Masterclass",
+    chef: "Chef Yukio Hattori",
+    level: "Intermediate",
+    price: 1500,
+    available: false,
+    image: "src/imgs/Yukio.jpg"
+},
+{
+    id: 2,
+    title: "Classic Italian Cuisine",
+    chef: "Chef Alessandro Giovanni",
+    level: "Beginner",
+    price: 2500,
+    available: false,
+    image: "src/imgs/Alessandro.jpeg"
+},
+{
+    id: 3,
+    title: "Seafood Masterclass",
+    chef: "Carlo Robertson",
+    level: "Advanced",
+    price: 5000,
+    available: true,
+    image: "src/imgs/carlo.jpg"
+},
+{
+    id: 4,
+    title: "Masterclass in Pastry",
+    chef: "Chef Amelia Garcia",
+    level: "Intermediate",
+    price: 1400,
+    available: true,
+    image: "src/imgs/Amelia.jpg"
+},
+{
+    id: 5,
+    title: "Roast Masterclass",
+    chef: "Amanda Gazelle",
+    level: "Beginner",
+    price: 1400,
+    available: true,
+    image: "src/imgs/Amanda.jpg"
+},
+{
+    id: 6,
+    title: "Masterclass of Desserts",
+    chef: "Chef Anna Maria",
+    level: "Intermediate",
+    price: 2200,
+    available: false,
+    image: "src/imgs/Anna.jpg"
+}
+]
